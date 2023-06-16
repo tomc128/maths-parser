26a0cb0a0e4f961ae4288eb38465921cc06ec89f
Tom Chapman <tomchapman128@gmail.com>
2023-06-16 22:28:03 +0100

    Add unit test project

---
+++ MathsParser.Test/ParserTests.cs
@@ -0,0 +1,15 @@
+namespace MathsParser.Test;
+
+public class ParserTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
