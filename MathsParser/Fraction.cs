54b874f95be8052327e6e52a2949345349ff9083
Tom Chapman <tomchapman128@gmail.com>
2023-06-16 18:35:36 +0100

    Add Fraction record

---
+++ MathsParser/Fraction.cs
@@ -0,0 +1,9 @@
+﻿namespace MathsParser;
+
+public readonly record struct Fraction(double Numerator, double Denominator)
+{
+    public override string ToString()
+    {
+        return Denominator == 1.0 ? $"{Numerator}" : $"{Numerator}/{Denominator}";
+    }
+}
